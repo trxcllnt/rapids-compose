ae417b6af3466364720e72cdb1cd109eb2024d25
ptaylor <paul.e.taylor@me.com>
2019-06-27 11:25:03 -0700

    fix flake8 error reporting

--- etc/check-style.sh
+++ etc/check-style.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -e
+#!/bin/bash
 
 # Run flake8 and get results/return code
 FLAKE=`flake8 python`
@@ -6,9 +6,9 @@ RETVAL=$?
 
 # Output results if failure otherwise show pass
 if [ "$FLAKE" != "" ]; then
-  echo -e "\n\n>>>> FAILED: flake8 style check; begin output\n\n"
-  echo -e "$FLAKE"
-  echo -e "\n\n>>>> FAILED: flake8 style check; end output\n\n"
+  echo -e "\n\n>>>> FAILED: flake8 style check; begin output\n\n" >&2
+  echo -e "$FLAKE" >&2
+  echo -e "\n\n>>>> FAILED: flake8 style check; end output\n\n" >&2
 else
   echo -e "\n\n>>>> PASSED: flake8 style check\n\n"
 fi
