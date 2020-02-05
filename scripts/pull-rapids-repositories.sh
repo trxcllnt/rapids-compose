5b986bdfd5e0e5c71fd1f0095bc0de9550e02d4a
Paul Taylor <paul.e.taylor@me.com>
2020-02-04 22:12:24 -0800

    also update submodules as necessary

--- scripts/pull-rapids-repositories.sh
+++ scripts/pull-rapids-repositories.sh
@@ -10,5 +10,8 @@ ALL_REPOS="${ALL_REPOS:-rmm cudf cugraph
                         notebooks notebooks-contrib}"
 
 for REPO in $ALL_REPOS; do
-    cd "$BASE_DIR/$REPO" && git pull upstream $(git branch --show-current) && cd -
+    cd "$BASE_DIR/$REPO" && \
+    git pull upstream $(git branch --show-current) && \
+    git submodule update --init --recursive && \
+    cd -
 done
--- scripts/push-rapids-repositories.sh
+++ scripts/push-rapids-repositories.sh
@@ -10,5 +10,7 @@ ALL_REPOS="${ALL_REPOS:-rmm cudf cugraph
                         notebooks notebooks-contrib}"
 
 for REPO in $ALL_REPOS; do
-    cd "$BASE_DIR/$REPO" && git push origin $(git branch --show-current) && cd -
+    cd "$BASE_DIR/$REPO" && \
+    git push origin $(git branch --show-current) && \
+    cd -
 done
