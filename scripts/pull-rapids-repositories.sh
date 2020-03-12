3687c2c8f9534205e7dd8387ab74822bcb768804
Paul Taylor <paul.e.taylor@me.com>
2020-03-12 10:58:30 -0700

    add prompt to enter branch name when current branch doesn't exist in upstream

--- scripts/pull-rapids-repositories.sh
+++ scripts/pull-rapids-repositories.sh
@@ -9,9 +9,27 @@ BASE_DIR="$(pwd)"
 CODE_REPOS="${CODE_REPOS:-rmm cudf cuml cugraph}"
 ALL_REPOS="${ALL_REPOS:-$CODE_REPOS notebooks notebooks-contrib}"
 
+
+
 for REPO in $ALL_REPOS; do
-    cd "$BASE_DIR/$REPO" && \
-    git pull upstream $(git rev-parse --abbrev-ref HEAD) && \
-    git submodule update --init --recursive && \
-    cd -
+    cd "$BASE_DIR/$REPO";
+    git fetch upstream && git fetch origin;
+    BRANCH_NAME="$(git rev-parse --abbrev-ref HEAD)";
+    while [ -z "$(git branch -r | grep upstream/$BRANCH_NAME)" ]; do
+        UPSTREAM_INFO="$(git remote -v show | grep upstream | head -n1)";
+        read -p "
+############################################################
+Branch \"$BRANCH_NAME\" not found in:
+${UPSTREAM_INFO}
+############################################################
+
+Please enter a branch name to pull (or leave empty to skip): " BRANCH_NAME </dev/tty
+    done
+    if [ -n "$BRANCH_NAME" ]; then
+        git pull upstream "$BRANCH_NAME";
+        git submodule update --init --recursive;
+    else
+        echo -e "No alternate branch name supplied, skipping\n";
+    fi;
+    cd - >/dev/null 2>&1;
 done
