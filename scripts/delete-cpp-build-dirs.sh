5f1232af2db3ddf9c474fa9185b64554ad2cd016
Paul Taylor <paul.e.taylor@me.com>
2020-03-31 18:44:48 -0700

    add helper to delete C++ build dirs

---
+++ scripts/delete-cpp-build-dirs.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+
+set -Eeo pipefail
+
+cd $(dirname "$(realpath "$0")")/../../
+
+BASE_DIR="$(pwd)"
+
+CPP_BUILD_DIRS="rmm/build
+                cudf/cpp/build
+                cuml/cpp/build
+                cugraph/cpp/build
+                cuspatial/cpp/build"
+
+ask_before_del() {
+    while true; do
+        read -p "$1 (default=$2) " CHOICE </dev/tty
+        if [ "$CHOICE" = "" ]; then CHOICE="$2"; fi;
+        case $CHOICE in
+            [Yy]* ) eval $3; break;;
+            [Nn]* ) eval $4; break;;
+            * ) echo "Please answer 'y' or 'n'";;
+        esac
+    done
+}
+
+echo "Deleting C++ build directories..."
+
+for DIR in $CPP_BUILD_DIRS; do
+    ask_before_del \
+        "Remove $BASE_DIR/$DIR (y/n)?" \
+        "N" \
+        "rm -rf $BASE_DIR/$DIR || true" \
+        "echo \"Skipping removing $DIR\""
+done
