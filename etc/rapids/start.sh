1361420be36bea0eb0c445a45d266df44c7bbea8
Karthikeyan <6488848+karthikeyann@users.noreply.github.com>
2020-06-11 14:45:59 +0530

    fix error missing functions

--- etc/rapids/start.sh
+++ etc/rapids/start.sh
@@ -16,7 +16,7 @@ set-gcc-version $GCC_VERSION >/dev/null 2>&1;
 source "$COMPOSE_HOME/etc/conda-install.sh" rapids
 
 # activate the rapids conda environment on bash login
-echo "source activate rapids && source \"$RAPIDS_HOME/.bashrc\"" > "$RAPIDS_HOME/.bash_login"
+echo "source \"$RAPIDS_HOME/compose/etc/bash-utils.sh && source activate rapids && source \"$RAPIDS_HOME/.bashrc\"" > "$RAPIDS_HOME/.bash_login"
 
 # If fresh conda env and cmd is build-rapids,
 # do `clean-rapids` to delete build artifacts
