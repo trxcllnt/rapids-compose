1341f636825c358123a6dd37bc1b8c06b9ba0591
ptaylor <paul.e.taylor@me.com>
2021-09-13 20:51:42 -0500

    update for latest vscode workspace integrated terminal profile settings

--- scripts/03-create-vscode-workspace.sh
+++ scripts/03-create-vscode-workspace.sh
@@ -257,9 +257,14 @@ cat << EOF
             "**/build/bdist.linux-x86_64*": true
         },
         "terminal.integrated.automationShell.linux": "/bin/bash",
-        "terminal.integrated.shell.linux": "/bin/bash",
         "terminal.integrated.enableFileLinks": true,
-        "terminal.integrated.shellArgs.linux": ["-c", "rapids_container=\$(docker ps | grep rapidsai/\$(whoami)/rapids | cut -d\" \" -f1 || echo \"\"); if [ \"\$rapids_container\" == \"\" ]; then exec \$SHELL; else exec docker exec -u \${UID}:\${GID} -it -w \"\$PWD\" \"\$rapids_container\" bash -li; fi"],
+        "terminal.integrated.profiles.linux": {
+            "bash": {
+                "path": "bash",
+                "icon": "terminal-bash",
+                "args": ["-c", "rapids_container=\$(docker ps | grep rapidsai/\$(whoami)/rapids | cut -d\" \" -f1 || echo \"\"); if [ \"\$rapids_container\" == \"\" ]; then exec \$SHELL; else exec docker exec -u \${UID}:\${GID} -it -w \"\$PWD\" \"\$rapids_container\" bash -li; fi"]
+            }
+        },
     },
     "tasks": {
         "version": "2.0.0",
