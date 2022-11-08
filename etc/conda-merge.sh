1900fc7bae8ae4b075dbef4a690e8c66f4585352
ptaylor <paul.e.taylor@me.com>
2022-11-08 10:45:52 -0800

    exclude raft-dask from packages to install

--- etc/conda-merge.sh
+++ etc/conda-merge.sh
@@ -84,7 +84,7 @@ conda-merge ${YMLS[@]} > merged.yml
 # Strip out cmake + the rapids packages, and save the combined environment
 cat merged.yml \
   | grep -v -P '^(.*?)\-(.*?)(rapids-build-env|rapids-notebook-env|rapids-doc-env|rapids-pytest-benchmark)(.*?)$' \
-  | grep -v -P '^(.*?)\-(.*?)(rmm|cudf|libraft|pyraft|dask-cudf|cugraph|cuspatial|cuxfilter)(.*?)$' \
+  | grep -v -P '^(.*?)\-(.*?)(rmm|cudf|libraft|pyraft|pylibraft|raft-dask|dask-cudf|cugraph|cuspatial|cuxfilter)(.*?)$' \
   | grep -v -P '^(.*?)\-(.*?)(\.git\@[^(main|master)])(.*?)$' \
   | grep -v -P '^(.*?)\-(.*?)(cmake=)(.*?)$' \
   > rapids.yml
