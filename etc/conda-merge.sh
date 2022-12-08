28495587c180602caf93f50676c4b1c165f7d24f
Vyas Ramasubramani <vyasr@nvidia.com>
2022-12-08 12:24:51 -0800

    Switch raft builds to use the dfg version of the env file. (#91)

--- etc/conda-merge.sh
+++ etc/conda-merge.sh
@@ -73,7 +73,7 @@ replace-env-versions-dfg() {
 
 YMLS=()
 if [ $(should-build-rmm)       == true ]; then echo -e "$(replace-env-versions-dfg rmm)"   > rmm.yml       && YMLS+=(rmm.yml);       fi;
-if [ $(should-build-raft)      == true ]; then echo -e "$(replace-env-versions raft)"      > raft.yml      && YMLS+=(raft.yml);      fi;
+if [ $(should-build-raft)      == true ]; then echo -e "$(replace-env-versions-dfg raft)"      > raft.yml      && YMLS+=(raft.yml);      fi;
 if [ $(should-build-cudf)      == true ]; then echo -e "$(replace-env-versions-dfg cudf)"  > cudf.yml      && YMLS+=(cudf.yml);      fi;
 if [ $(should-build-cuml)      == true ]; then echo -e "$(replace-env-versions cuml)"      > cuml.yml      && YMLS+=(cuml.yml);      fi;
 if [ $(should-build-cugraph)   == true ]; then echo -e "$(replace-env-versions cugraph)"   > cugraph.yml   && YMLS+=(cugraph.yml);   fi;
